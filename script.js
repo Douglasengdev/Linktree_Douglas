21704e6bdf8fce174f885555381e0e1f272b05d0
Douglas Medeiros <131925326+Douglasengdev@users.noreply.github.com>
2024-02-25 11:16:27 -0300

    Atualização de arquivos
    Mudança de todo arquivo do projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+function alerta()
+{
+    swal({
+        title: "Ops...",
+        text: "O portifólio ainda está em desenvolvimento, entre em contato com o desenvolvedor para saber mais!",
+        icon: "warning",
+        button: "Certo!",
+      });
+}
